b0afc4ff4e053bf6fcc438d259c93f3fd1b41a39
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-07 23:57:31 +0530

    Two Sum II - Input array is sorted
    Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
    
    The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
    
    Note:
    
    Your returned answers (both index1 and index2) are not zero-based.
    You may assume that each input would have exactly one solution and you may not use the same element twice.
    Example:
    
    Input: numbers = [2,7,11,15], target = 9
    Output: [1,2]
    Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.

---
+++ Arrays and Strings/12.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+    
+        int i = 0 ;
+        int j = numbers.length -1 ;
+        
+        while ( i <= j ) {
+            int sum = numbers[i] + numbers[j];
+            
+            if ( sum > target ) {
+                j-- ;
+            } 
+            else if ( sum < target ) {
+                i++ ;
+            } 
+            else {
+                return new int[] { i + 1 , j + 1 };
+            }
+        } 
+    
+        return new int[] { i + 1 , j + 1 };
+    }
+}
