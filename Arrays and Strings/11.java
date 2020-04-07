d4a13e264b6e5435a98e6d1234faee44eefedf9e
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-07 21:59:39 +0530

    Array Partition I
    Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
    
    Example 1:
    Input: [1,4,3,2]
    
    Output: 4
    Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).

---
+++ Arrays and Strings/11.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        
+        int sum = 0 ;
+        
+        Arrays.sort( nums ) ;
+        
+        for ( int i = 0 ; i < nums.length ; i = i + 2 ) {
+            
+            sum += Math.min( nums[i] , nums[i+1]) ;
+        }
+        
+        return sum ;
+        
+        
+    }
+}
