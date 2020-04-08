b46089763c732d9ac000766e3367796fa6b24d96
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-08 09:20:29 +0530

    Rotating Array

--- Arrays and Strings/16.java
+++ Arrays and Strings/16.java
@@ -20,3 +20,23 @@ class Solution {
         }
     }
 }
+
+*******************************************************************
+//Reverse method solution
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length ;
+        reverse( nums, 0, nums.length - 1) ;
+        reverse( nums, 0, k - 1 ) ;
+        reverse( nums, k, nums.length - 1 ) ;
+    }
+    
+    public void reverse ( int[] a , int start, int end ) {
+       while ( start < end ) { 
+        int temp = a[start] ;
+        a[start++] = a[end] ;
+        a[end--] = temp ;
+       }
+    }
+}
