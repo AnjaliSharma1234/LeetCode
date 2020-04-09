58a86f161c5a3ac9bd077e28ebd8965976c17cb2
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-09 15:45:50 +0530

    Remove Duplicates from Sorted Array
    Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
    
    Example 1:
    
    Given nums = [1,1,2],
    
    Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
    It doesn't matter what you leave beyond the returned length.
    Example 2:
    
    Given nums = [0,0,1,1,1,2,2,3,3,4],
    Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
    It doesn't matter what values are set beyond the returned length.

---
+++ Arrays and Strings/20.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int size = 1 ;
+        
+        for ( int i = 0 ; i < nums.length - 1 ; i++ ) {
+        
+           
+        if ( nums[i + 1] != nums[i] ) {
+            
+            nums[ size++ ] = nums[ i + 1 ] ;
+        }
+            
+            
+        }
+        
+        return size ; 
+        
+    }
+}
