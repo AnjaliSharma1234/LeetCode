41cb2cafe6ea1d3295710a9d1dcaa425c61ebada
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-09 14:36:59 +0530

    Reverse Words in a String
    Given an input string, reverse the string word by word.
    Example 1:
    
    Input: "the sky is blue"
    Output: "blue is sky the"
    Example 2:
    
    Input: "  hello world!  "
    Output: "world! hello"
    Explanation: Your reversed string should not contain leading or trailing spaces.

---
+++ Arrays and Strings/18.js
@@ -0,0 +1,9 @@
+class Solution {
+    public String reverseWords(String s) {
+        
+        String[] words = s.trim().split("\\s+");
+        Collections.reverse(Arrays.asList(words)) ;
+        return String.join(" ", words);
+
+    }
+}
